f3c8218333e2642001b5f9c8ecba50bb7430ecdb
Bulat Khayrullin <bulat.khairullin@akvelon.com>
2021-08-31 12:55:15 +0300

    basic graphql server with initial schema

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express = require("express");
-const app = express();
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const { readFileSync } = require("fs");
+const { notes, authors } = require('./utils/api');
+
+const schemaString = readFileSync('./schema.graphql', { encoding: 'utf-8'});
+const schema = buildSchema(schemaString);
+
+
+const root = {
+    getAllNotesByAuthor: (params) => {
+        return notes.filter(({ authorId }) => params.id === authorId);
+    },
+    getNote: (params) => {
+        return notes.find(({ id }) => params.id === id);
+    },
+    addNote: (params) => {
+        notes.push({
+            id: notes.length, 
+            ...params.note,
+            authorId: params.authorId,
+        });
+        return true;
+    },
+}
 
 const PORT = 5000 || process.env.PORT;
-
+const app = express();
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
+);
 app.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`)
 });
---
+++ utils/api.js
@@ -0,0 +1,65 @@
+exports.notes = [
+    {
+        id: '1',
+        title: '1st note',
+        text: 'Text of the 1st note',
+        authorId: '1',
+    },
+    {
+        id: '2',
+        title: '2nd note',
+        text: 'Text of the 2nd note',
+        authorId: '1',
+    },   
+    {
+        id: '3',
+        title: '3rd note',
+        text: 'Text of the 3rd note',
+        authorId: '2',
+    },    
+    {
+        id: '4',
+        title: '4th note',
+        text: 'Text of the 4th note',
+        authorId: '2',
+    },
+    {
+        id: '5',
+        title: '5th note',
+        text: 'Text of the 5th note',
+        authorId: '2',
+    },
+    {
+        id: '6',
+        title: '6th note',
+        text: 'Text of the 6th note',
+        authorId: '2',
+    },
+    {
+        id: '7',
+        title: '7th note',
+        text: 'Text of the 7th note',
+        authorId: '3',
+    },
+    {
+        id: '8',
+        title: '8th note',
+        text: 'Text of the 8th note',
+        authorId: '3',
+    },
+];
+
+exports.authors = [
+    {
+        id: '1',
+        username: 'alexey.kordakov',
+    },
+    {
+        id: '2',
+        username: 'ivan.petrov',
+    },
+    {
+        id: '3',
+        username: 'michail.alexandrov',
+    },
+]
